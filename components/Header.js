21ffc0c1c722e8804d7a089fb2e7edad7a02c4d2
YeoJiSu <duwltn1301@naver.com>
2022-10-24 17:57:34 +0900

    style : header 수정

--- components/Header.js
+++ components/Header.js
@@ -12,17 +12,17 @@ const Header = () => {
     return (
        
     <div>
-            <header className={styles.header}>
-                <div class = {styles.head}>
-                    <a href="/"> <Image src={img} className={styles.logo} alt="jisutagram" width={120} height={30}  /> </a>
-                    <ul className={styles.ul}>
-                        <li className={styles.li}> <a href="https://github.com/YeoJiSu"> <Image src={git} alt="Vercel Logo" width={35} height={35}  /> </a></li>
-                        <li className={styles.li}> <a href="https://velog.io/@diduya">V</a> </li>
-                        <li className={styles.li}> <a href="https://www.youtube.com/channel/UCzjT4g6JGcuvsns7EIYHUwQ"> <Image src={you} alt="Vercel Logo" width={40} height={30}  /> </a></li>
-                        <li className={styles.li}> <a href="/"> <Image className={styles.img3} src={img3} alt="Vercel Logo" width={40} height={40}  /> </a></li>
-                    </ul>
-                </div>
-            </header>
+        <header className={styles.header}>
+            <div class = {styles.head}>
+                <a href="/"> <Image src={img} className={styles.logo} alt="jisutagram" width={120} height={30}  /> </a>
+                <ul className={styles.ul}>
+                    <li className={styles.li}> <a href="https://github.com/YeoJiSu"> <Image src={git} alt="Vercel Logo" width={35} height={35}  /> </a></li>
+                    <li className={styles.li}> <a href="https://velog.io/@diduya">V</a> </li>
+                    <li className={styles.li}> <a href="https://www.youtube.com/channel/UCzjT4g6JGcuvsns7EIYHUwQ"> <Image src={you} alt="Vercel Logo" width={40} height={30}  /> </a></li>
+                    <li className={styles.li}> <a href="/"> <Image className={styles.img3} src={img3} alt="Vercel Logo" width={40} height={40}  /> </a></li>
+                </ul>
+            </div>
+        </header>
         <div className={styles.header2}>
             <div className={styles.div1}>
                 <div className={styles.div2}>
@@ -33,13 +33,19 @@ const Header = () => {
                     jisu Yeo
                   </p>
                   <p className={styles.div5}>
-                    📭 yeoji1503@gmail.com<br/>
-                    📭 duwltn1301@pusan.ac.kr<br/>
-                    📭 duwltn1301@naver.com
+                    '솔루션 아키텍트'가 되고 싶은 여지수입니다.
+                  </p>
+                  
+                  <p className={styles.div6}>
+                    📭 yeoji1503@gmail.com
+                  </p>
+                  <p className={styles.div6}>
+                    📱 010-0000-0000
                   </p>
                 </div>
             </div>
         </div>
+       
     </div>
     );
 };
