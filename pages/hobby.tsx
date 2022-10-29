9d59d14ab34f9e69c54ad9986956f4da36d96768
YeoJiSu <duwltn1301@naver.com>
2022-10-29 21:54:34 +0900

    hobby page 내용 생성

--- pages/hobby.tsx
+++ pages/hobby.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Layout from '../components/Layout';
+import { useRouter } from "next/dist/client/router";
 const Hobby: NextPage = () => {
+
+  const router = useRouter();
+
   return (
-    <div className={styles.container}>
+    <Layout>
+      <div className={styles.container}>
+      <main className={styles.main}>
+      <div className={styles.grid}>
       
-    </div>
+        <button className = {styles.btn} onClick={()=>router.push('/hobby/horse-riding')}>🏇🏻</button>
+        <button className = {styles.btn} onClick={()=>router.push('/hobby/drawing')}>🎨</button>
+        <button className = {styles.btn} onClick={()=>router.push('/hobby/baking')}>👩🏻‍🍳</button>
+        <button className = {styles.btn} onClick={()=>router.push('/hobby/playing-piano')}>🎹</button>
+        <button className = {styles.btn} onClick={()=>router.push('/hobby/taking-photos')}>📸</button>
+
+
+        
+      </div>
+      </main>
+      </div>
+    </Layout>
   );
 };
 
